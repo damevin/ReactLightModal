0553a1317aad33a7973209d272ed855741a7a4d2
raphaeldamevin <72107589+damevin@users.noreply.github.com>
2022-01-13 14:38:18 +0100

    🧪  Add tests

--- src/components/Modal/Modal.test.tsx
+++ src/components/Modal/Modal.test.tsx
@@ -4,7 +4,23 @@ import { render } from "@testing-library/react";
 import Modal from "./Modal";
 
 describe("Modal", () => {
-  test("renders the Modal component", () => {
-    render(<Modal title="Hello world !"content="Hello world!" quitOnEsc overlay />);
-  });
+	test("renders the Modal component", () => {
+		render(
+			<Modal
+				title="Hello world !"
+				canClose
+				isOpen={true}
+				toggleModal={() => {}}
+				closeBtnContent={<p>X</p>}
+				haveFooter={false}
+				footerContent={<></>}
+				modalContent={
+					<>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere inventore a impedit et
+						expedita vero esse repellat, quasi reiciendis tempora?
+					</>
+				}
+			/>
+		);
+	});
 });
